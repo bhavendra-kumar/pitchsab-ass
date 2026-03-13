f8f3941e89973c5dde78679a5e804f934cf49817
bhavendra <bhavendrakumar007@gmail.com>
2026-03-13 23:50:23 +0530

    done

--- frontend/src/Pages/LandingPage.jsx
+++ frontend/src/Pages/LandingPage.jsx
@@ -1,15 +1,14 @@
-import GridBackground from "../components/GridBackground";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Features from "../components/Features";
-import HowItWorks from "../components/HowItWorks";
-import CTA from "../components/CTA";
-import Footer from "../components/Footer";
+import GridBackground from "../components/GridBackground"
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import Features from "../components/Features"
+import HowItWorks from "../components/HowItWorks"
+import CTA from "../components/CTA"
+import Footer from "../components/Footer"
 
 function LandingPage() {
   return (
     <div className="relative min-h-screen bg-[#0f172a] text-white">
-
       <GridBackground />
 
       <Navbar />
